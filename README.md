275bab0e6417fb897287ef034635747cc39acd8d
Corben Leo <corben.leo@citrix.com>
2019-07-21 13:34:43 -0400

    initial push

---
+++ README.md
@@ -0,0 +1,12 @@
+# otxurls
+Fetch known URLs from AlienVault's [Open Threat Exchange](https://otx.alienvault.com) for given hosts.
+
+### usage:
+```
+▻ printf 'example.com' | otxurls
+```
+
+### install:
+```
+▻ go get github.com/lc/otxurls
+```
---
+++ otxurls.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+type OTXResult struct {
+	HasNext    bool `json:"has_next"`
+	ActualSize int  `json:"actual_size"`
+	URLList    []struct {
+		Domain   string `json:"domain"`
+		URL      string `json:"url"`
+		Hostname string `json:"hostname"`
+		Httpcode int    `json:"httpcode"`
+		PageNum  int    `json:"page_num"`
+		FullSize int    `json:"full_size"`
+		Paged    bool   `json:"paged"`
+	} `json:"url_list"`
+}
+
+var (
+	c = &http.Client{
+		Timeout: time.Second * 15,
+	}
+	domains []string
+	url     []string
+)
+
+func main() {
+	flag.Parse()
+	if flag.NArg() > 0 {
+		domains = []string{flag.Arg(0)}
+	} else {
+		s := bufio.NewScanner(os.Stdin)
+		for s.Scan() {
+			domains = append(domains, s.Text())
+		}
+	}
+	for _, domain := range domains {
+		page := 0
+		for {
+			r, err := c.Get(fmt.Sprintf("https://otx.alienvault.com/api/v1/indicators/hostname/%s/url_list?limit=50&page=%d", domain, page))
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			defer r.Body.Close()
+			bytes, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			o := &OTXResult{}
+			err = jsoniter.Unmarshal(bytes, o)
+			if err != nil {
+				log.Fatalf("Could not decode json: %s\n", err)
+			}
+			for _, url := range o.URLList {
+				fmt.Println(url.URL)
+			}
+			if !o.HasNext {
+				break
+			}
+			page++
+		}
+	}
+}
